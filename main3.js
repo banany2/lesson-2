7a43f632443b5ac7b7791062edbd23d68da53d61
banany2 <d.petruk16@gmail.com>
2024-05-10 20:36:22 +0300

    added a js script

---
+++ main3.js
@@ -0,0 +1,5 @@
+let userInput1 = prompt("Enter the first row:")
+let userInput2 = prompt("Enter the second row:")
+let userInput3 = prompt("Enter the third row:")
+
+console.log("The result is: ${userInput1}, ${userInput2}, ${userInput3}")
