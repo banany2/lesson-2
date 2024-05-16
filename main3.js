e79f79f0efd2f63752cda006e09a4a3ec591055b
banany2 <d.petruk16@gmail.com>
2024-05-16 15:46:50 +0300

    added a new functional

--- main3.js
+++ main3.js
@@ -1,16 +1,26 @@
-let numOrStr = prompt("Input number or string");
-console.log(numOrStr)
-
-switch (numOrStr) {
-    case numOrStr === null:
-        console.log("ви скасували");
-        break;
-    case numOrStr.trim() === "":
-            console.log("Empty String");
-        break;
-    case isNaN( +numOrStr):
-        console.log("number is Ba_NaN");
-        break;
-    default: console.log("OK!");
+let Year = prompt("In what year were you born?");
+let Place = prompt("Where do you live?");
+let favouriteSport = prompt("What is your favourite sport?");
+switch (true) {
+    case Year === null:
+        alert("It's a pity that you didn't enter your year of birth(");
+break;
+    case Place === null:
+        alert("It's a pity that you didn't enter the place, where you live(");
+break;
+    case favouriteSport === null:
+        alert("It's a pity that you didn't enter your favourite sport(");
+break;
 }
-
+if (Place==="Washington") {
+    alert (`You were born in ${Year}. You live in the capital of USA. Your favourite sport is ${favouriteSport}.`)
+}
+else if (Place==="Kyiv") {
+    alert (`You were born in ${Year}. You live in the capital of Ukraine. Your favourite sport is ${favouriteSport}.`)
+}
+else if (Place==="London") {
+    alert(`You were born in ${Year}. You live in the capital of England. Your favourite sport is ${favouriteSport}.`)
+}
+else {
+    alert(`You were born in ${Year}. You live in ${Place}. Your favourite sport is ${favouriteSport}.`)
+}
