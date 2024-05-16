f1346be87593e282573069d6e2f6a0c5b96427b3
banany2 <d.petruk16@gmail.com>
2024-05-16 11:23:32 +0300

    added a new functional

--- main3.js
+++ main3.js
@@ -1,2 +1,22 @@
-let Name = prompt("Як Вас зовуть?")
-alert(`Hello, ${Name}! How are you?`)
+let Numbers = prompt("Enter a three-digit number:");
+let Symbols = Numbers.split('');
+console.log(Symbols);
+if (Numbers.length < 3 || Numbers.length > 3) {
+
+    console.log("invalid input.");
+
+}
+else if(Numbers[0] === Numbers[1] && Numbers[1] === Numbers[2]){
+    console.log("Every number is the same.")
+}
+else if(Numbers[0] === Numbers[1]){
+    console.log("The first and the second number is the same.")
+}
+else if(Numbers[1] === Numbers[2]){
+    console.log("The second and the third number is the same.")
+}
+else if(Numbers[0] === Numbers[2]){
+    console.log("The first and the third number is the same.")
+}
+
+
