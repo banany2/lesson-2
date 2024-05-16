b192098e112bc98111055fc458c34d2145bf798b
banany2 <d.petruk16@gmail.com>
2024-05-16 10:45:32 +0300

    created hometask

--- main3.js
+++ main3.js
@@ -1,5 +1,2 @@
-let userInput1 = prompt("Enter the first row:")
-let userInput2 = prompt("Enter the second row:")
-let userInput3 = prompt("Enter the third row:")
-
-console.log(`The result is: ${userInput1}, ${userInput2}, ${userInput3}`)
+let Name = prompt("Як Вас зовуть?")
+alert(`Hello, ${Name}! How are you?`)
