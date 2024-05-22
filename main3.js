774e055cad6a34cb03c4c9c6bce37fbd6a6c068c
banany2 <d.petruk16@gmail.com>
2024-05-22 17:10:46 +0300

    added changes

--- main3.js
+++ main3.js
@@ -1,10 +1,9 @@
-let n = prompt("Enter an integer number");
-for (let x = 1; n <= 100; x++) {
-    let square = x * x;
-    if (square <= n) {
-        console.log(x);
+let userNum = prompt("Enter number:");
+let a, b;
+go:
+    for (a = 2; a <= userNum; a++) {
+        for (b = 2; b < a; b++) {
+            if (a%b === 0) continue go;
+        }
+        console.log(a)
     }
-    else {
-        break;
-    }
-}
