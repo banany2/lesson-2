169566a54f4cb7a71c8f7e4aa13063b5495568f1
banany2 <d.petruk16@gmail.com>
2024-05-30 16:53:13 +0300

    added

--- main3.js
+++ main3.js
@@ -1,13 +1,14 @@
-function removeLetters(str, charsToRemove) {
- let result = "";
- for (let i = 0; i < str.length; i++) {
-  if (!charsToRemove.includes(str[i])) {
-   result += str[i];
-  }
- }
- return result;
+function createCounter(count) {
+    count = 0
+    return function(num) {
+        count += num;
+        console.log(count)
+    }
 }
-const inputString = prompt("Enter the string:");
-const lettersToRemove = prompt("Enter the characters to remove (e.g., 'ld'):").split('');
-const resultString = removeLetters(inputString, lettersToRemove);
-console.log("Result:", resultString);
+
+const sum = createCounter();
+console.log(sum(2))
+console.log(sum(3))
+console.log(sum(6))
+
+
