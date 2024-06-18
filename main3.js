ca464fe19dd8c8d615285a0ea8215e356bae427d
banany2 <d.petruk16@gmail.com>
2024-06-18 03:06:26 +0300

    added

--- main3.js
+++ main3.js
@@ -1,5 +1,34 @@
-let newArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let contactBook = {
+    contacts: [
+        {
+            name: 'Dima',
+            phone: '+1234567890',
+            email: 'Dima@gmail.com',
+        },
+        {
+            name: 'Vasya',
+            phone: '+0987654321',
+            email: 'Vasya@gmail.com',
+        },
+        {
+            name: 'Kirill',
+            phone: '+1122334455',
+            email: 'kirill@gmail.com',
+        }
+    ],
+    searchName: function(name) {
+        return this.contacts.find(contact => contact.name === name)
+    },
+    addContact: function(name, phone, email) {
+        if(this.contacts.find(contact => contact.name === name)) {
+            return 'contact with this name is already exists'
+        }
+        this.contacts.push({ name, phone, email });
+        console.log('Contact is added:')
+        return `${name} (${phone}) ${email}`;
+    }
 
-const evenNums = newArr.filter(nums=>nums % 2 === 0)
+};
 
-console.log(evenNums)
+console.log(contactBook.searchName('Dima'));
+console.log(contactBook.addContact('Toxa', '+2131523567', 'Toxa@gmail.com'));
