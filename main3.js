f6a16bc9e4d6cbca51b0ca5a910be7f48c7a7b66
banany2 <d.petruk16@gmail.com>
2024-06-20 15:50:20 +0300

    added

--- main3.js
+++ main3.js
@@ -1,34 +1,10 @@
-let contactBook = {
-    contacts: [
-        {
-            name: 'Dima',
-            phone: '+1234567890',
-            email: 'Dima@gmail.com',
-        },
-        {
-            name: 'Vasya',
-            phone: '+0987654321',
-            email: 'Vasya@gmail.com',
-        },
-        {
-            name: 'Kirill',
-            phone: '+1122334455',
-            email: 'kirill@gmail.com',
-        }
-    ],
-    searchName: function(name) {
-        return this.contacts.find(contact => contact.name === name)
-    },
-    addContact: function(name, phone, email) {
-        if(this.contacts.find(contact => contact.name === name)) {
-            return 'contact with this name is already exists'
-        }
-        this.contacts.push({ name, phone, email });
-        console.log('Contact is added:')
-        return `${name} (${phone}) ${email}`;
+const btnForce = document.getElementById('btn')
+let newColor = false
+btnForce.addEventListener('click', () => {
+    if(newColor) {
+        btnForce.style.color = 'red'
+    } else {
+        btnForce.style.color = 'green'
     }
-
-};
-
-console.log(contactBook.searchName('Dima'));
-console.log(contactBook.addContact('Toxa', '+2131523567', 'Toxa@gmail.com'));
+    newColor = !newColor
+})
