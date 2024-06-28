de1e864c272bbf9509430d65c087629aa458277f
banany2 <d.petruk16@gmail.com>
2024-06-28 10:05:09 +0300

    added

--- main3.js
+++ main3.js
@@ -1,8 +1,25 @@
-let btnContainer = document.querySelector('.btn-container')
-btnContainer.addEventListener('click', function() {
-    if (event.target.tagName === 'BUTTON') {
-        alert(`You clicked: ${event.target.textContent}`)
+let task = document.getElementById('task-list')
+let newTaskInput = document.getElementById('newTaskInput')
+let addBtn = document.getElementById('add-task')
+
+function addTask() {
+    const taskText = newTaskInput.value;
+    if (taskText === '') return;
+
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Видалити';
+    deleteBtn.className = 'delete-btn';
+    taskItem.appendChild(deleteBtn);
+    task.appendChild(taskItem);
+    newTaskInput.value = '';
+}
+
+addBtn.addEventListener('click', addTask);
+task.addEventListener('click', function(event) {
+    if (event.target.classList.contains('delete-btn')) {
+        const taskItem = event.target.parentNode;
+        task.removeChild(taskItem);
     }
 })
-
-
