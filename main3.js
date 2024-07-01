1609bb8b676a10ec4b33d62067e8d0cddc134047
banany2 <d.petruk16@gmail.com>
2024-07-02 01:58:02 +0300

    added

--- main3.js
+++ main3.js
@@ -1,25 +1,73 @@
-let task = document.getElementById('task-list')
-let newTaskInput = document.getElementById('newTaskInput')
-let addBtn = document.getElementById('add-task')
+const emailInput = document.getElementById('email')
+const messageSpan = document.getElementById('messageError')
+const nameErrorSpan = document.getElementById('nameError')
+const nameInput = document.getElementById('name')
+const textareaInput = document.getElementById('textarea')
+const textareaErrorSpan = document.getElementById('textareaError')
+const form = document.getElementById('myForm')
+const phoneInput = document.getElementById('phone')
+const phoneErrorSpan = document.getElementById('phoneError')
 
-function addTask() {
-    const taskText = newTaskInput.value;
-    if (taskText === '') return;
-
-    const taskItem = document.createElement('li');
-    taskItem.textContent = taskText;
-    const deleteBtn = document.createElement('button');
-    deleteBtn.textContent = 'Видалити';
-    deleteBtn.className = 'delete-btn';
-    taskItem.appendChild(deleteBtn);
-    task.appendChild(taskItem);
-    newTaskInput.value = '';
-}
-
-addBtn.addEventListener('click', addTask);
-task.addEventListener('click', function(event) {
-    if (event.target.classList.contains('delete-btn')) {
-        const taskItem = event.target.parentNode;
-        task.removeChild(taskItem);
+phoneInput.addEventListener('input', function(event) {
+    const phoneValue = event.target.value
+    if (phoneValue !== null && /^\+\d+$/.test(phoneValue) && phoneValue.length >= 10 && phoneValue.length <= 13 && phoneValue.includes('+')) {
+        phoneErrorSpan.innerText = ''
+    } else {
+        phoneErrorSpan.style.color = 'red'
+        phoneErrorSpan.innerText = 'Here must be 13 characters long and starts with +380'
     }
 })
+
+textareaInput.addEventListener('input', function(event) {
+    const textareaValue = event.target.value
+    if (textareaValue.length <= 5) {
+        textareaErrorSpan.style.color = 'red'
+        textareaErrorSpan.innerText = 'Here must be more than 5 characters'
+    } else {
+        textareaErrorSpan.innerText = ''
+    }
+})
+
+nameInput.addEventListener('input', function(event) {
+    const name = event.target.value
+    if (name === '') {
+        nameErrorSpan.style.color = 'red'
+        nameErrorSpan.innerText = 'the name is required'
+    } else {
+        nameErrorSpan.style.color = ''
+        nameErrorSpan.innerText = ''
+    }
+})
+
+emailInput.addEventListener('input', function (event) {
+    const email = event.target.value;
+    if (email.includes('@') && email.includes('.')) {
+        messageSpan.innerText = '';
+    } else {
+        messageSpan.style.color = 'red'
+        messageSpan.innerText = 'Email must contain . and @'
+
+    }
+
+})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    if (phoneErrorSpan.innerText !== '' || textareaErrorSpan.innerText !== '' || nameErrorSpan.innerText !== '' || messageSpan.innerText !== '') {
+        console.log('Form submission prevented due to errors.');
+        return;
+    }
+
+
+    const formData = {
+        name: nameInput.value,
+        email: emailInput.value,
+        phone: phoneInput.value,
+        message: textareaInput.value
+    }
+    console.log(formData)
+})
+
+
+
