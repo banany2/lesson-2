49afc921c0f93e53776b10c520e195840bc2859a
banany2 <d.petruk16@gmail.com>
2024-07-13 00:25:01 +0300

    added

--- main3.js
+++ main3.js
@@ -1,75 +1,33 @@
-function createStudent(name, surname, birthYear, grades = []) {
-    return {
-        name: name,
-        surname: surname,
-        birthYear: birthYear,
-        grades: grades,
-        attendance: Array(25).fill(null),
-
-        getAge: function() {
-            const currentYear = new Date().getFullYear()
-            return currentYear - this.birthYear
-        },
-
-        getAverageGrade: function () {
-            if (this.grades.length === 0) return 0;
-            const sum = this.grades.reduce((sum, grade) => sum + grade, 0)
-            return sum / this.grades.length
-        },
-
-        present: function() {
-            const index = this.attendance.indexOf(null);
-            if (index !== -1) {
-                this.attendance[index] = true;
+class Calculator {
+    add(num1, num2) {
+        if (!isNaN(num1) && !isNaN(num2)) {
+            return num1 + num2
+        }
+    }
+    subtract(num1, num2) {
+        if (!isNaN(num1) && !isNaN(num2)) return num1 - num2
+    }
+    multiply(num1, num2) {
+        if (!isNaN(num1) && !isNaN(num2)) return num1 * num2
+    }
+    divide(num1, num2) {
+        if (!isNaN(num1) && !isNaN(num2)) {
+            if (num2 === 0) {
+                return 'cannot divide by 0'
             } else {
-                console.log('Масив відвідуваності заповнений');
-            }
-        },
-
-        absent: function() {
-            const index = this.attendance.indexOf(null);
-            if (index !== -1) {
-                this.attendance[index] = false;
-            } else {
-                console.log('Масив відвідуваності заповнений');
-            }
-        },
-
-        summary: function() {
-            const totalClasses = this.attendance.filter(item => item !== null).length;
-            const attendedClasses = this.attendance.filter(item => item === true).length;
-            const averageAttendance = totalClasses === 0 ? 0 : attendedClasses / totalClasses;
-            const averageGrade = this.getAverageGrade();
-
-            if (averageGrade > 90 && averageAttendance > 0.9) {
-                return "Молодець!";
-            } else if (averageGrade > 90 || averageAttendance > 0.9) {
-                return "Добре, але можна краще";
-            } else {
-                return "Редиска!";
+                return num1 / num2
             }
         }
-    };
+        return 'invalid input'
+    }
 }
 
-const student1 = createStudent('Іван', 'Іванов', 2000, [95, 85, 92, 88, 90]);
-const student2 = createStudent('Петро', 'Петров', 1999, [80, 75, 78, 82, 85]);
-const student3 = createStudent('Марія', 'Марієнко', 2001, [60, 65, 70, 72, 68]);
+const calc = new Calculator()
 
-student1.present();
-student1.present();
-student1.absent();
-console.log(`${student1.name} ${student1.surname}, вік: ${student1.getAge()}, середній бал: ${student1.getAverageGrade()}`);
-console.log(student1.summary());
+console.log(calc.add(3, 6)); // 8
 
-student2.present();
-student2.absent();
-student2.present();
-console.log(`${student2.name} ${student2.surname}, вік: ${student2.getAge()}, середній бал: ${student2.getAverageGrade()}`);
-console.log(student2.summary());
+console.log(calc.subtract(10, 4)); // 6
 
-student3.absent();
-student3.absent();
-student3.present();
-console.log(`${student3.name} ${student3.surname}, вік: ${student3.getAge()}, середній бал: ${student3.getAverageGrade()}`);
-console.log(student3.summary());
+console.log(calc.multiply(3, 6)); // 18
+
+console.log(calc.divide(8, 2)); // 4
