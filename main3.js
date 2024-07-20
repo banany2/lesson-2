e880aa0aeb3ae4d3edcbfa61b1078d9c417fd2cb
banany2 <d.petruk16@gmail.com>
2024-07-20 23:49:16 +0300

    added

--- main3.js
+++ main3.js
@@ -1,28 +1,24 @@
-let startTime = 300;
-let timerInterval = null
-function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${String(minutes).padStart(2, '0')}:${String(secs).padStart(2, '0')}`;
+async function getWeather() {
+    const apiKey = '5a0edee8c31a54a21752c521198fbd81'
+    const city = 'Odessa'
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`;
+
+    fetch(url)
+        .then(function (resp) {
+            return resp.json();
+        })
+        .then(function (data) {
+            console.log(data)
+            let imgSrc = `https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png`
+            document.querySelector('.city-name').innerHTML = data.name
+            document.querySelector('.degrees').innerHTML = `${Math.round(data.main.temp)}&deg;C`
+            document.querySelector('.weather-description').innerHTML = data.weather[0]['description']
+            document.querySelector('img').innerHTML = imgSrc
+        })
+
+        .catch(function (error) {
+            console.log(error)
+        })
+
 }
-
-function updateTimer() {
-    const timerElement = document.getElementById('timer');
-    timerElement.textContent = formatTime(startTime);
-
-    if (startTime > 0) {
-        startTime--;
-    } else {
-        clearInterval(timerInterval);
-    }
-}
-
-function startTimer() {
-    if (timerInterval !== null) {
-        return;
-    }
-    updateTimer();
-    timerInterval = setInterval(updateTimer, 1000);
-}
-
-document.getElementById('startBtn').addEventListener('click', startTimer);
+getWeather()
